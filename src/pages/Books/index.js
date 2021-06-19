834208a08288e7a7f751c0a5e6dc2cd3e6d38aa0
rodolfochagas <rodolfochagas@id.uff.br>
2021-06-19 19:41:33 -0300

    Page Books: removed mocks and loaded page using url, implemented delete and logout

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -1,96 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 import logoImage from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
+import api from '../../services/api';
 
 export default function Books() {
+
+    const [books, setBooks] = useState([]);
+
+    const userName = localStorage.getItem('userName');
+    const accessToken = localStorage.getItem('accessToken');
+    const history = useHistory();
+
+    useEffect(() => {
+        api.get('api/Book/v1/asc/25/1', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            setBooks(response.data.list);
+        });
+
+    }, [accessToken]);
+
+    async function deleteBook(id) {
+        try {
+            await api.delete(`api/Book/v1/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            setBooks(books.filter(book => book.id !== id))
+
+        } catch (error) {
+            alert("Delete failed! Please try again.");
+        }
+    }
+
+    async function logout() {
+        try {
+            await api.get('api/auth/v1/revoke', {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            history.push('/');
+            localStorage.clear();
+            
+        } catch (error) {
+            alert("Logout failed! Please try again.")
+        }
+    }
+
     return (
         <div className="books-container">
             <header>
                 <img src={logoImage} alt="logo"/>
-                <span>Welcome, <strong>Fulano</strong>!</span>
+                <span>Welcome, <strong>{userName}</strong>!</span>
                 <Link className="button" to="book/new">Add new book</Link>
                 <button type="button">
-                    <FiPower size={18} color="#251FC5"></FiPower>
+                    <FiPower size={18} color="#251FC5" onClick={logout}></FiPower>
                 </button>
             </header>
 
             <h1>Registered Books</h1>
             <ul>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
+                {books.map(book => {
+                    return (
+                        <li key={book.id}>
+                            <strong>Title:</strong>
+                            <p>{book.title}</p>
+                            <strong>Author:</strong>
+                            <p>{book.author}</p>
+                            <strong>Price:</strong>
+                            <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(book.price)}</p>
+                            <strong>Release Date:</strong>
+                            <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
 
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5"></FiEdit>
-                    </button>
+                            <button type="button">
+                                <FiEdit size={20} color="#251FC5"></FiEdit>
+                            </button>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5"></FiTrash2>
-                    </button>
-                </li>
-
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5"></FiEdit>
-                    </button>
-                    
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5"></FiTrash2>
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5"></FiEdit>
-                    </button>
-                    
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5"></FiTrash2>
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5"></FiEdit>
-                    </button>
-                    
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5"></FiTrash2>
-                    </button>
-                </li>
+                            <button type="button" onClick={() => deleteBook(book.id)}>
+                                <FiTrash2 size={20} color="#251FC5"></FiTrash2>
+                            </button>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     )
